6dd856bb2eccf946d199d729076b2ea8c84795df
pedro <armbox@pedros-MacBook-Air.local>
2024-08-18 00:18:59 -0300

    floating action buttons

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,8 +9,43 @@ import 'package:provider/provider.dart';
 import 'package:macro_tracker/providers/macro_provider.dart';
 import 'package:macro_tracker/widgets/macro_summary.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  HomeScreenState createState() => HomeScreenState();
+}
+
+class HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  bool _isExpanded = false;
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 250),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  void _toggleExpand() {
+    setState(() {
+      _isExpanded = !_isExpanded;
+      if (_isExpanded) {
+        _animationController.forward();
+      } else {
+        _animationController.reverse();
+      }
+    });
+  }
 
   bool _hasNaN(Macro macros) {
     return macros.carb.isNaN || macros.fat.isNaN || macros.protein.isNaN;
@@ -104,7 +139,8 @@ class HomeScreen extends StatelessWidget {
                     actions: [
                       TextButton(
                         onPressed: () {
-                          macroProvider.clearDailyMacros();
+                          Provider.of<MacroProvider>(context, listen: false)
+                              .clearDailyMacros();
                           Navigator.of(context).pop();
                         },
                         child: const Text('Yes'),
@@ -143,15 +179,86 @@ class HomeScreen extends StatelessWidget {
                 ],
               ),
             ),
-      floatingActionButton: FloatingActionButton(
-        heroTag: 'addMeal',
-        onPressed: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(builder: (context) => const AddMealEntryScreen()),
-          );
-        },
-        tooltip: 'Add Meal',
-        child: const Icon(Icons.add),
+      floatingActionButton: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          _buildAnimatedFAB(
+            context,
+            icon: Icons.restaurant,
+            label: 'New Meal',
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                    builder: (context) => const AddMealEntryScreen()),
+              );
+            },
+            animationValue: 2,
+          ),
+          const SizedBox(height: 16),
+          _buildAnimatedFAB(
+            context,
+            icon: Icons.fastfood,
+            label: 'New Food',
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => const AddFoodScreen()),
+              );
+            },
+            animationValue: 1,
+          ),
+          const SizedBox(height: 16),
+          FloatingActionButton(
+            onPressed: _toggleExpand,
+            tooltip: 'Add',
+            child: Icon(_isExpanded ? Icons.close : Icons.add),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildAnimatedFAB(
+    BuildContext context, {
+    required IconData icon,
+    required String label,
+    required VoidCallback onPressed,
+    required int animationValue,
+  }) {
+    return AnimatedSlide(
+      offset: _isExpanded ? const Offset(0, 0) : const Offset(0, 1),
+      duration: const Duration(milliseconds: 250),
+      curve: Curves.easeInOut,
+      child: AnimatedOpacity(
+        opacity: _isExpanded ? 1.0 : 0.0,
+        duration: const Duration(milliseconds: 250),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Material(
+              borderRadius: BorderRadius.circular(8.0),
+              elevation: 4.0,
+              color: Colors.white,
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                child: Text(
+                  label,
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(width: 8),
+            FloatingActionButton(
+              heroTag: label,
+              onPressed: onPressed,
+              mini: true,
+              child: Icon(icon),
+            ),
+          ],
+        ),
       ),
     );
   }
